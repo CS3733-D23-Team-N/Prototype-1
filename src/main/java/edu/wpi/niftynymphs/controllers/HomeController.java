23a839ce59282d676da49193832bac3065c80785
LianruiSun <darrrrrrrrrcy@gmail.com>
2023-03-28 18:44:17 -0400

    a complete homepage

--- src/main/java/edu/wpi/niftynymphs/controllers/HomeController.java
+++ src/main/java/edu/wpi/niftynymphs/controllers/HomeController.java
@@ -2,32 +2,35 @@ package edu.wpi.niftynymphs.controllers;
 
 import edu.wpi.niftynymphs.navigation.Navigation;
 import edu.wpi.niftynymphs.navigation.Screen;
-import io.github.palexdev.materialfx.controls.MFXButton;
 import java.awt.*;
 import javafx.fxml.FXML;
 
 public class HomeController {
-  @FXML MFXButton conferenceRoomButton;
-  @FXML MFXButton mealDeliveryButton;
-  @FXML MFXButton flowerDeliveryButton;
-  @FXML MFXButton FurnitureDeliveryButton;
-  @FXML MFXButton OfficeDeliveryButton;
 
   @FXML
-  public void initialize() {
-    conferenceRoomButton.setOnMouseClicked(event ->
-            Navigation.navigate(
-                Screen.SERVICE_REQUEST)); // conference room reservation request button
-    mealDeliveryButton.setOnMouseClicked(
-        event -> Navigation.navigate(Screen.SERVICE_REQUEST)); // meal delivery request button
-    flowerDeliveryButton.setOnMouseClicked(
-        event -> Navigation.navigate(Screen.SERVICE_REQUEST)); // flower delivery request button
-    FurnitureDeliveryButton.setOnMouseClicked(
-            event -> Navigation.navigate(Screen.SERVICE_REQUEST)); // furniture delivery request button
-    OfficeDeliveryButton.setOnMouseClicked(
-            event -> Navigation.navigate(Screen.SERVICE_REQUEST)); // office delivery request button
+  public void initialize() {}
 
-    // request button
+  public void navigateConferenceRoom() {
+    Navigation.navigate(Screen.SERVICE_REQUEST);
+  }
 
+  public void navigateMealDelivery() {
+    Navigation.navigate(Screen.SERVICE_REQUEST);
+  }
+
+  public void navigateFlowerDelivery() {
+    Navigation.navigate(Screen.SERVICE_REQUEST);
+  }
+
+  public void navigateFurnitureDelivery() {
+    Navigation.navigate(Screen.SERVICE_REQUEST);
+  }
+
+  public void navigateOfficeSupply() {
+    Navigation.navigate(Screen.SERVICE_REQUEST);
+  }
+
+  public void navigateSignage() {
+    Navigation.navigate(Screen.SERVICE_REQUEST);
   }
 }
