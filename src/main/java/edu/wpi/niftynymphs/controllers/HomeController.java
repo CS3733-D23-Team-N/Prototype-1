a8269af7a78e5dec79607073a8ff3527fe3e4dbb
LianruiSun <darrrrrrrrrcy@gmail.com>
2023-03-28 18:09:00 -0400

    committing to pull front end

--- src/main/java/edu/wpi/niftynymphs/controllers/HomeController.java
+++ src/main/java/edu/wpi/niftynymphs/controllers/HomeController.java
@@ -3,14 +3,31 @@ package edu.wpi.niftynymphs.controllers;
 import edu.wpi.niftynymphs.navigation.Navigation;
 import edu.wpi.niftynymphs.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import java.awt.*;
 import javafx.fxml.FXML;
 
 public class HomeController {
-
-  @FXML MFXButton navigateButton;
+  @FXML MFXButton conferenceRoomButton;
+  @FXML MFXButton mealDeliveryButton;
+  @FXML MFXButton flowerDeliveryButton;
+  @FXML MFXButton FurnitureDeliveryButton;
+  @FXML MFXButton OfficeDeliveryButton;
 
   @FXML
   public void initialize() {
-    navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+    conferenceRoomButton.setOnMouseClicked(event ->
+            Navigation.navigate(
+                Screen.SERVICE_REQUEST)); // conference room reservation request button
+    mealDeliveryButton.setOnMouseClicked(
+        event -> Navigation.navigate(Screen.SERVICE_REQUEST)); // meal delivery request button
+    flowerDeliveryButton.setOnMouseClicked(
+        event -> Navigation.navigate(Screen.SERVICE_REQUEST)); // flower delivery request button
+    FurnitureDeliveryButton.setOnMouseClicked(
+            event -> Navigation.navigate(Screen.SERVICE_REQUEST)); // furniture delivery request button
+    OfficeDeliveryButton.setOnMouseClicked(
+            event -> Navigation.navigate(Screen.SERVICE_REQUEST)); // office delivery request button
+
+    // request button
+
   }
 }
