6bea992434756174b70debcc2c7fae89fa82b2ab
Harshith Iyer <harbar2021@gmail.com>
2023-03-29 14:15:02 -0400

    fixed home controller, added dummy onClick methods to meal delivery

--- src/main/java/edu/wpi/niftynymphs/controllers/MealServiceRequestController.java
+++ src/main/java/edu/wpi/niftynymphs/controllers/MealServiceRequestController.java
@@ -1,16 +1,38 @@
 package edu.wpi.niftynymphs.controllers;
 
-import edu.wpi.niftynymphs.navigation.Navigation;
-import edu.wpi.niftynymphs.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Popup;
 
 public class MealServiceRequestController {
 
-  @FXML MFXButton backButton;
+  @FXML MFXButton pizzaButton;
+  @FXML MFXButton pastaButton;
+  @FXML MFXButton chocolateCakeButton;
+
+  @FXML
+  public void onPizzaClicked() {
+    System.out.println("Hello");
+  }
+
+  @FXML
+  public void onPastaClicked() {
+    System.out.println("Hello");
+  }
+
+  @FXML
+  public void onChocolateCakeClicked() {
+    System.out.println("Hello");
+  }
+
+  @FXML
+  public void onSubmit() {
+    System.out.println("Hello");
+  }
 
   @FXML
   public void initialize() {
-    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+
   }
 }
